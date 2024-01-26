7a121bdd75fcec79d0d0ab1e45582f53781739fa
Hilmi Yafi A <56539775+hilmiyafia@users.noreply.github.com>
2024-01-26 13:14:59 +0700

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -1,45 +1,32 @@
-def position(control_points, time):
-   position_x = (
-      1 * ((1 - time) ** 3) * (time ** 0) * control_points[0][0] +
-      3 * ((1 - time) ** 2) * (time ** 1) * control_points[1][0] +
-      3 * ((1 - time) ** 1) * (time ** 2) * control_points[2][0] +
-      1 * ((1 - time) ** 0) * (time ** 3) * control_points[3][0]
-   )
-   position_y = (
-      1 * ((1 - time) ** 3) * (time ** 0) * control_points[0][1] +
-      3 * ((1 - time) ** 2) * (time ** 1) * control_points[1][1] +
-      3 * ((1 - time) ** 1) * (time ** 2) * control_points[2][1] +
-      1 * ((1 - time) ** 0) * (time ** 3) * control_points[3][1]
-   )
-   return [position_x, position_y]
+def position(controls, time):
+   a = 1 * ((1 - time) ** 3) * (time ** 0)
+   b = 3 * ((1 - time) ** 2) * (time ** 1)
+   c = 3 * ((1 - time) ** 1) * (time ** 2)
+   d = 1 * ((1 - time) ** 0) * (time ** 3)
+   x = a * controls[0][0] + b * controls[1][0] + c * controls[2][0] + d * controls[3][0]
+   y = a * controls[0][1] + b * controls[1][1] + c * controls[2][1] + d * controls[3][1]
+   return x, y
 
-def speed(control_points, time, epsilon=1e-6):
-   velocity_x = (
-      3 * ((1 - time) ** 2) * (time ** 0) * (control_points[1][0] - control_points[0][0]) +
-      6 * ((1 - time) ** 1) * (time ** 1) * (control_points[2][0] - control_points[1][0]) +
-      3 * ((1 - time) ** 0) * (time ** 2) * (control_points[3][0] - control_points[2][0])
-   )
-   velocity_y = (
-      3 * ((1 - time) ** 2) * (time ** 0) * (control_points[1][1] - control_points[0][1]) +
-      6 * ((1 - time) ** 1) * (time ** 1) * (control_points[2][1] - control_points[1][1]) +
-      3 * ((1 - time) ** 0) * (time ** 2) * (control_points[3][1] - control_points[2][1])
-   )
-   return (velocity_x ** 2 + velocity_y ** 2) ** 0.5 + epsilon
+def speed(controls, time):
+   a = 3 * ((1 - time) ** 2) * (time ** 0)
+   b = 6 * ((1 - time) ** 1) * (time ** 1)
+   c = 3 * ((1 - time) ** 0) * (time ** 2)
+   x = a * (controls[1][0] - controls[0][0]) + b * (controls[2][0] - controls[1][0]) + c * (controls[3][0] - controls[2][0])
+   y = a * (controls[1][1] - controls[0][1]) + b * (controls[2][1] - controls[1][1]) + c * (controls[3][1] - controls[2][1])
+   return (x ** 2 + y ** 2) ** 0.5
 
-def split(control_points, interval, epsilon=1e-2, maximum_iteration=100):
-   time   = [0]
-   points = [control_points[0]]
+def split(controls, interval, epsilon=1e-2, maximum_iteration=100):
+   times = [0]
+   points = [controls[0]]
    while True:
-      delta_time = interval / speed(control_points, time[-1])
+      delta_time = interval / (speed(controls, times[-1]) + 1e-6)
       for iteration in range(maximum_iteration):
-         new_time    = time[-1] + delta_time
-         new_point   = position(control_points, new_time)
+         new_time = times[-1] + delta_time
+         new_point = position(controls, new_time)
          delta_point = ((new_point[0] - points[-1][0]) ** 2 + (new_point[1] - points[-1][1]) ** 2) ** 0.5
-         if abs(delta_point - interval) < epsilon:
-            break
-         delta_time *= (interval / delta_point + 1) / 2
-      if new_time > 1:
-         break
-      time.append(new_time)
+         if abs(delta_point - interval) < epsilon: break
+         delta_time *= 0.5 * (interval / delta_point + 1)
+      if new_time > 1: break
+      times.append(new_time)
       points.append(new_point)
-   return time, points
+   return times, points
